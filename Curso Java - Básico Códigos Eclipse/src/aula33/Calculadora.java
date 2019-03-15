9342f27f5a6f6dd86411dd9ca168b9f10bf4934c
Islane <islaness.yes@gmail.com>
2019-03-15 15:52:39 -0300

    Orientação a objetos - exemplo de sobrecarga

---
+++ Curso Java - Básico Códigos Eclipse/src/aula33/Calculadora.java
@@ -0,0 +1,28 @@
+package aula33;
+
+public class Calculadora {
+
+	public int soma(int n1, int n2) {
+
+		return n1 + n2;
+	}
+
+	public int soma(int n1, int n2, int n3) {
+
+		return n1 + n2 + n3;
+	}
+	
+	public double soma (double n1, double n2) {
+		
+		return n1+n2;
+	}
+	public int soma (int [] vetorInteiros) {
+		
+		int total = 0;
+		for (int i = 0; i < vetorInteiros.length; i++) {
+			total+= vetorInteiros[i];
+		}
+		return total;
+	}
+
+}
---
+++ Curso Java - Básico Códigos Eclipse/src/aula33/testeCalculadora.java
@@ -0,0 +1,15 @@
+package aula33;
+
+public class testeCalculadora {
+
+	public static void main(String[] args) {
+
+
+		Calculadora c = new Calculadora();
+		
+		System.out.println(c.soma(1, 2));
+		System.out.print(c.soma(2.9,9.6));
+
+	}
+
+}
