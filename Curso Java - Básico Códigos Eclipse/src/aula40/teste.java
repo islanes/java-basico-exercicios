ba779e53271139a28172063207a0dfa9582a6b75
Islane <islaness.yes@gmail.com>
2019-03-18 15:47:14 -0300

    Polimorfismo - sobrecarga de metodos

---
+++ Curso Java - Básico Códigos Eclipse/src/aula40/teste.java
@@ -0,0 +1,29 @@
+
+package aula40;
+
+/**
+ *
+ * @author Islane dos S. Silva
+ */
+public class teste {
+
+    public static void main(String[] args) {
+        
+        Pessoa pessoa = new Pessoa();
+        Pessoa aluno = new Aluno();
+        Pessoa professor = new Professor();
+        
+        //Aluno aluno = new Aluno();
+        
+        pessoa.setEndereco("Rua 1, n 1");
+        aluno.setEndereco("Rua 2, n 1");
+        professor.setEndereco("Rua 1, n 9");
+        
+        System.out.println(""+pessoa.obterEtiquetaEndereco());
+        System.out.println(""+aluno.obterEtiquetaEndereco());
+        System.out.println(""+professor.obterEtiquetaEndereco());
+        
+       // aluno.getCurso();
+        
+    }
+}
