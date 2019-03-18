fee23f03884f1c080dbb13546302725f46189932
Islane <islaness.yes@gmail.com>
2019-03-18 16:36:34 -0300

    A palavra chave final

---
+++ Curso Java - Básico Códigos Eclipse/src/aula42/Aluno.java
@@ -0,0 +1,75 @@
+package aula42;
+
+/**
+ *
+ * @author Islane dos S. Silva
+ */
+public class Aluno {
+
+    private String curso;
+    private double[] notas;
+
+    public Aluno() {
+    }
+
+    public Aluno(String nome, String endereco, String telefone, String curso) {
+       // super(nome, endereco, telefone);//acesso a superClasse -> Pessoa
+        this.curso = curso;
+    }
+
+    public void metodoQualquer() {
+       // super.getCpf();//da classe pessoa
+
+    }
+
+    public double calcularMedia() {
+        double media = 0;
+        return media;
+    }
+
+    public boolean verificarAprovado() {
+        return true;
+    }
+
+    /**
+     * @return the curso
+     */
+    public String getCurso() {
+        return curso;
+    }
+
+    /**
+     * @param curso the curso to set
+     */
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    /**
+     * @return the notas
+     */
+    public double[] getNotas() {
+        return notas;
+    }
+
+    /**
+     * @param notas the notas to set
+     */
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public String obterEtiquetaEndereco() {
+        //return endereco;
+        //sobreescrevendo o metodo
+        String s = "Endereço aluno: ";
+        //s += this.getEndereco();
+        return s;
+    }
+
+    //@Override
+    public void imprimirEtiquetaEndereco() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("" + this.obterEtiquetaEndereco());
+    }
+}
---
+++ Curso Java - Básico Códigos Eclipse/src/aula42/teste.java
@@ -0,0 +1,19 @@
+package aula42;
+
+/**
+ *
+ * @author Islane dos S. Silva
+ */
+public class teste {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+        /*Pessoa aluno = new Aluno();
+        Pessoa professor = new Professor();*/
+        
+        Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+
+    }
+}
