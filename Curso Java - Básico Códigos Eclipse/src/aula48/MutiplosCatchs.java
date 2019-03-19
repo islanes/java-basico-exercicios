716c669ba437249c7027b458234105af5f942196
Islane <islaness.yes@gmail.com>
2019-03-19 20:06:52 -0300

    Exceção Mútiplos Catchs

---
+++ Curso Java - Básico Códigos Eclipse/src/aula48/MutiplosCatchs.java
@@ -0,0 +1,26 @@
+package aula48;
+
+/**
+ *
+ * @author Islane dos S. Silva
+ */
+public class MutiplosCatchs {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {4, 8, 16, 32, 64, 128};
+        int[] denominadores = {2, 0, 4, 8, 0};
+
+        for (int i = 0; i < numeros.length; i++) {
+            try {
+                System.out.println(numeros[i] + " / " + denominadores[i] + " = " + (numeros[i] / denominadores[i]));
+
+            }catch(ArithmeticException exception){
+                System.out.println("Erro ao dividir por ZERO");
+            }catch(Throwable exception){
+                System.out.println("Ocorreu um erro");
+            }
+        }
+
+    }
+}
